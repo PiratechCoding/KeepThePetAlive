f75adcdc0bf2f5adf5139ce201b83700d53b07fb
PiratechCoding <lukebucherschool@gmail.com>
2021-01-05 16:14:58 -0500

    Initial start commit with transparent overlay

---
+++ KeepThePetAlive/KeepThePetAlive/KeepThePetAlive.cpp
@@ -0,0 +1,186 @@
+// KeepThePetAlive.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "KeepThePetAlive.h"
+
+#define MAX_LOADSTRING 100
+// Static Declartation of UI Elements
+
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_KEEPTHEPETALIVE, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_KEEPTHEPETALIVE));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_KEEPTHEPETALIVE));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(BLACK_BRUSH);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_KEEPTHEPETALIVE);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    
+    return RegisterClassExW(&wcex);
+}
+
+
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+ 
+   const int WINDOWHEIGHT = 400;
+   const int WINDOWWIDITH = 300;
+   int windowStartX = GetSystemMetrics(SM_CXSCREEN) - WINDOWWIDITH;
+   int windowStartY = GetSystemMetrics(SM_CYSCREEN) - WINDOWHEIGHT;
+   HWND hcentralWindow = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED,
+      windowStartX, windowStartY, WINDOWWIDITH, WINDOWHEIGHT, nullptr, nullptr, hInstance, nullptr);
+   
+   // Set WS_EX_LAYERED on this window
+   SetWindowLongW(hcentralWindow, GWL_EXSTYLE, GetWindowLong(hcentralWindow, GWL_EXSTYLE) | WS_EX_LAYERED);
+   SetLayeredWindowAttributes(hcentralWindow, 0, 255*5/100, LWA_ALPHA);
+   SetMenu(hcentralWindow, 0);
+   SetWindowPos(hcentralWindow, HWND_TOPMOST, windowStartX, windowStartY, WINDOWWIDITH, WINDOWHEIGHT, SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOMOVE);
+   
+   if (!hcentralWindow)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hcentralWindow, nCmdShow);
+   UpdateWindow(hcentralWindow);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
